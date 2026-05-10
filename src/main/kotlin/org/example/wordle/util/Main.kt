6ddacd6d877089f82b5a72b34e062002ab07ffdd
mikaschulz <msz@fam-schulz-hamburg.de>
2026-05-11 00:32:40 +0200

    Remove legacy Java sources (migrated to Kotlin)

---
+++ src/main/kotlin/org/example/wordle/util/Main.kt
@@ -0,0 +1,12 @@
+package org.example.wordle.util
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        IO.println("Hello and welcome!")
+        for (i in 1..5) {
+            IO.println("i = $i")
+        }
+    }
+}
+
